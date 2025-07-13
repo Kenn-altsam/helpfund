4732870c4dae01e31ea00d6304262d7036e0eff7
Kenn-altsam <your_email@example.com>
2025-07-13 13:12:16 +0500

    update

--- backend/src/chats/models.py
+++ backend/src/chats/models.py
@@ -12,6 +12,8 @@ class Chat(Base):
     # Make sure 'users.id' matches the primary key of your User model.
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
     openai_thread_id = Column(String, nullable=True, index=True, unique=True)
+
+    openai_assistant_id = Column(String, nullable=True, index=True) 
     
     title = Column(String(255), nullable=False)
     
